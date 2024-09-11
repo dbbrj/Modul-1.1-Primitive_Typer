5b2901cb2ea8fcdcc491c95967fdb3302a4192f7
Ditte Jensen <dbbrj@hotmail.com>
2024-09-11 06:57:00 +0200

    Lavet exersices 5.1-5.3

---
+++ Exercises_5.1-5.3/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Den beste datatype for temperatur er Float");
+Console.WriteLine("Den medste datatype for måned er Int");
