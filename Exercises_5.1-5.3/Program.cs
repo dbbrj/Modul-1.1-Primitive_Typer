f2210ca7e98a4976186eee4976ec9cd9063580a2
Ditte Jensen <dbbrj@hotmail.com>
2024-09-11 11:00:21 +0200

    slet

--- Exercises_5.1-5.3/Program.cs
+++
@@ -1,4 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Den beste datatype for temperatur er Float");
-Console.WriteLine("Den medste datatype for måned er Int");
