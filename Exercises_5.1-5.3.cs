3b8349f69d0361f2e12ada6d4cd50deb52bd1daf
Ditte Jensen <dbbrj@hotmail.com>
2024-09-11 18:24:11 +0200

    New files

--- Exercises_5.1-5.3.cs
+++ Exercises_5.1-5.3.cs
@@ -1,1 +1,12 @@
-Console.WriteLine ("Hello, World!");
+using System;
+
+class Program
+{
+	static void Main()
+	{
+		Console.WriteLine("Hello, World");
+		Console.WriteLine("The best datatype for tempeture is \"float\"");
+		Console.WriteLine("The best datatype for month is \"byte\"");
+	}
+}
+
